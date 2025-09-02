cdb744d91e20ceea0c59fe52801a57b0a7bc5098
Posadasx7 <posadashne@gmail.com>
2025-09-02 12:53:31 -0600

    Actualizar Verdad.js

--- plugins/Verdad.js
+++ plugins/Verdad.js
@@ -22,7 +22,7 @@ const handler = async (msg, { conn }) => {
     await conn.sendMessage(chatId, {
       video: { url: 'https://cdn.russellxz.click/ee5ab947.mp4' },
       gifPlayback: true,
-      caption: `𝘏𝘢𝘴 𝘦𝘴𝘤𝘰𝘨𝘪𝘥𝘰 *𝘝𝘌𝘙𝘋𝘈𝘋*\n\n╱╲❀╱╲╱╲❀╱╲╱╲❀╱╲\n◆ ${verdad}\n╲╱❀╲╱╲╱❀╲╱╲╱❀╲╱\n\n© La Suki Bot`
+      caption: `𝘏𝘢𝘴 𝘦𝘴𝘤𝘰𝘨𝘪𝘥𝘰 *𝘝𝘌𝘙𝘋𝘈𝘋*\n\n╱╲❀╱╲╱╲❀╱╲╱╲❀╱╲\n◆ ${verdad}\n╲╱❀╲╱╲╱❀╲╱╲╱❀╲╱\n\n© Michi Ice`
     }, { quoted: msg });
 
     await conn.sendMessage(chatId, {
